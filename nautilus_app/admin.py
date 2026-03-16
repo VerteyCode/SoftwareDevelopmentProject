c4c0195251d46cd0314185fd2a0d345f90d223cd
VerteyCode <danieljones54451@gmail.com>
2026-03-16 13:54:10 +0000

    Registered models in admin panel

--- nautilus_app/admin.py
+++ nautilus_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import NautilusTeamModel, NautilusReviewModel, NautilusConsultationModel
+
+admin.site.register(NautilusTeamModel)
+admin.site.register(NautilusReviewModel)
+admin.site.register(NautilusConsultationModel)
